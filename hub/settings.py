30f1d108731f1a4a2dcce6876129388632370957
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-08-22 19:10:14 +0530

    reset

--- hub/settings.py
+++ hub/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'view.apps.ViewConfig',
-    'rest_framework'
+    'courseAPI.apps.CourseapiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
