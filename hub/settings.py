205efa8bbc9021d8ad8ec2d8ecc9849f046a0972
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-08-31 00:58:47 +0530

    Update settings.py

--- hub/settings.py
+++ hub/settings.py
@@ -129,7 +129,5 @@ COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-        os.path.join(BASE_DIR,'hub/static'),
-] 
+STATICFILES_DIRS =[] 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
